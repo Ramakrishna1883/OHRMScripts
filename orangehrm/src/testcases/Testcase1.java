8979db0373a4fca3b8cbdfb4d245af45cefe1c2c
Lenovo <Lenovo@DESKTOP-Q2EKAVS>
2022-08-31 12:05:55 +0530

    changed testcase1

--- orangehrm/src/testcases/Testcase1.java
+++ orangehrm/src/testcases/Testcase1.java
@@ -3,6 +3,6 @@ package testcases;
 public class Testcase1 
 {
 public static void main(String[] args) {
-	System.out.println("This is tetcase1");
+	System.out.println("This is tetcase1 added");
 }
 }
